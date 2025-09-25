6bf99230198f01db0fbd33c83c4083d6f5e29a9c
sameehaafaaf <sameeha.afaaf@gmail.com>
2025-09-25 11:08:30 +0800

    Update Q2.0.cpp

--- Q2.0.cpp
+++ Q2.0.cpp
@@ -14,9 +14,8 @@ int main() {
         else if (speed == 0){
             cout << "Stopped" << endl;
         }
-        else {
+        else 
             cout << "Moving backward" << endl;
-        }  
     }
       /**
      * HOMEWORK TODO 2: repeat the following code indefinitely-- while, ending before return 0;
